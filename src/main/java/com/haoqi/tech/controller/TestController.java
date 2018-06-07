98dd957fb59f0654d9e6faf68b19219bb8dc8482
suzhengxiao <suzhengxiao@jd.com>
2018-06-07 15:29:26 +0800

    test @SpringBootApplication

---
+++ src/main/java/com/haoqi/tech/controller/TestController.java
@@ -0,0 +1,7 @@
+package com.haoqi.tech.controller;
+
+/**
+ * Created by suzhengxiao on 2018/6/7.
+ */
+public class TestController {
+}
---
+++ src/main/java/com/haoqi/tech/dto/TestDTO.java
@@ -0,0 +1,7 @@
+package com.haoqi.tech.dto;
+
+/**
+ * Created by suzhengxiao on 2018/6/7.
+ */
+public class testDTO {
+}
