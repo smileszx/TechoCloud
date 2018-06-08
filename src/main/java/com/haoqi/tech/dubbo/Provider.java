988624d2dfe081c44a283cb49b95ec91f5c5bab1
suzhengxiao <suzhengxiao@jd.com>
2018-06-08 11:36:49 +0800

    add dubbo rpc

---
+++ src/main/java/com/haoqi/tech/dubbo/Provider.java
@@ -0,0 +1,11 @@
+package com.haoqi.tech.dubbo;
+
+public class Provider {
+    public static void main(String[] args) throws Exception {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                new String[] {"META-INF/spring/dubbo-demo-provider.xml"});
+        context.start();
+        // press any key to exit
+        System.in.read();
+    }
+}
---
+++ src/main/java/com/haoqi/tech/dubbo/provider/DubboServiceImpl.java
@@ -0,0 +1,7 @@
+package com.haoqi.tech.dubbo.provider;
+
+/**
+ * Created by suzhengxiao on 2018/6/8.
+ */
+public class DubboServiceImpl {
+}
