7f6773bb6ad8d953583d37b6f22687f5f528a397
suzhengxiao <suzhengxiao@jd.com>
2018-07-19 09:38:48 +0800

    change merge file

--- src/test/java/com/haoqi/tech/GitMergeTest.java
+++ src/test/java/com/haoqi/tech/GitMergeTest.java
@@ -4,4 +4,7 @@ package com.haoqi.tech;
  * Created by suzhengxiao on 2018/7/19.
  */
 public class GitMergeTest {
+    public static void main(String[] args) {
+        System.out.println("Merge test!");
+    }
 }
