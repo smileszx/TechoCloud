eefda9eb03b1048e935d3cc46ee5edbd9f8c274f
smile <smile.szx@outlook.com>
2019-01-02 09:49:58 +0800

    test -> git rebase commit

--- src/test/java/com/haoqi/tech/countdown/CountDownTest.java
+++ src/test/java/com/haoqi/tech/countdown/CountDownTest.java
@@ -10,21 +10,27 @@ public class CountDownTest {
         Thread t1 = new Thread(() -> {
             try {
                 Thread.sleep(1000);
-                threads.await();
+                System.out.println("test1");
             } catch (InterruptedException e) {
                 e.printStackTrace();
+            } finally {
+                threads.countDown();
             }
         });
+        t1.start();
 
         Thread t2 = new Thread(()-> {
             try {
                 Thread.sleep(1000);
-                threads.await();
+                System.out.println("test2");
             } catch (InterruptedException e) {
                 e.printStackTrace();
+            } finally {
+                threads.countDown();
             }
         });
-
+        t2.start();
+        threads.countDown();
         threads.await();
         System.out.println("test end");
     }
